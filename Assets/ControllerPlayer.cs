c7075bd67b7cf6d92ce0ec04532730da2979c785
Tom <tom.kcz@hotmail.fr>
2017-09-16 00:37:37 +0200

    Commit

---
+++ Assets/ControllerPlayer.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Rewired;
+
+public class ControllerPlayer : MonoBehaviour {
+
+    private Player player;
+
+
+    void Awake()
+    {
+        player = ReInput.players.GetPlayer(0);
+    }
+
+	// Use this for initialization
+	void Start () {
+        
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        if (player.GetButtonDown("Yo"))
+        {
+            Debug.Log(player);
+        }
+
+    }
+}
---
+++ Assets/Scripts/Camera/ScreenShake.cs
@@ -0,0 +1,112 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using DG.Tweening;
+
+public class ScreenShake : MonoBehaviour
+{
+	public static ScreenShake Singleton;
+
+	void Awake ()
+	{
+		if (ScreenShake.Singleton == null) {
+			ScreenShake.Singleton = this;
+		} else {
+			Destroy (gameObject);
+		}
+	}
+
+	public void ShakeHit (float side)
+	{
+		side = Mathf.Clamp (side, -2f, 2f);
+		transform.DOKill (true);
+
+		transform.DOPunchRotation (Vector3.forward * side * 0.4f, .3f, 1, 1);
+		transform.DOPunchPosition (Vector3.one * 0.4f, .3f, 1, 0.5f);
+	}
+
+    public void DoubleRoastHit(float side)
+    {
+        side = Mathf.Clamp(side, -1f, 1f);
+        transform.DOKill(true);
+
+        transform.DOPunchRotation(Vector3.forward * side * 0.2f, .3f, 1, 1);
+        transform.DOPunchPosition(Vector3.one * 0.2f, .3f, 1, 0.5f);
+    }
+
+
+    public void ProtectHit(float side)
+    {
+        side = Mathf.Clamp(side, -2f, 2f);
+        transform.DOKill(true);
+
+        transform.DOPunchRotation(Vector3.forward * side * 0.4f, .15f, 1, .95f);
+        transform.DOPunchPosition(Vector3.one * 0.4f, .25f, 1, .4f);
+    }
+
+
+    public void Win (float side)
+	{
+		transform.DOShakeRotation (2f, 2f, 15, 90);
+		transform.DOShakePosition (2f, 2f, 15, 90);
+	}
+
+    public void Destroy()
+    {
+        transform.DOShakeRotation(1f, .9f, 22, 90);
+        transform.DOShakePosition(1f, .9f, 22, 90);
+    }
+
+    public void Respawn()
+    {
+        transform.DOShakeRotation(.5f, 1.4f, 17, 180);
+        transform.DOShakePosition(.5f, 1.4f, 17, 180);
+    }
+
+    public void StartPropulsion (float side)
+	{
+		side = Mathf.Clamp (side, -2f, 2f);
+		transform.DOKill (true);
+		transform.DORotate (transform.forward * side * 1.5f, .35f).SetEase (Ease.InOutExpo);
+	}
+
+    public void StartThumbsUp(float side)
+    {
+        side = Mathf.Clamp(side, -20f, 20f);
+        transform.DOKill(true);
+        transform.DORotate(transform.forward * side * 2f, .15f).SetEase(Ease.InOutExpo);
+
+        ScreenShake.Singleton.GetComponentInChildren<Camera>().DOOrthoSize(3.5f, .8f);
+        DOVirtual.DelayedCall(2f, () =>
+        {
+            ScreenShake.Singleton.GetComponentInChildren<Camera>().DOOrthoSize(4f, 1f);
+            transform.DOKill(true);
+            transform.DORotate(Vector3.zero, .5f);
+            /*DOVirtual.DelayedCall(7f, () =>
+            {
+                transform.DOKill(true);
+                transform.DORotate(Vector3.zero, .5f);
+            });*/
+        });
+    }
+
+    public void EndThumbsUp()
+    {
+        transform.DOKill(true);
+        transform.DORotate(Vector3.zero, 1f).SetEase(Ease.InOutExpo);
+    }
+
+    public void EndPropulsion ()
+	{
+		transform.DOKill (true);
+		transform.DORotate (Vector3.zero, 1f).SetEase (Ease.InOutExpo);
+	}
+
+    public void MenuShake()
+    {
+        transform.DOKill(true);
+
+        transform.DOPunchRotation(Vector3.one * 0.4f, .3f, 1, 1);
+        transform.DOPunchPosition(Vector3.one * 0.4f, .3f, 1, 0.5f);
+    }
+}
